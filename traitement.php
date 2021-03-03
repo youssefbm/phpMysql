0508c6dd93ca781ed8fad8a9020cab98892f1df4
youssef <yo.ouadid@gmail.com>
2021-03-03 18:28:27 +0100

    algorithm in traitement file

--- traitement.php
+++ traitement.php
@@ -2,33 +2,14 @@
 
 include_once('database.php');
 
-// verifier si le message est envoyé correctement
+// si le message est envoyé correctement
+    // récupérer les données dans des variables
+    // si les valeurs récupéré sont vides
+        //se redériger ver la page index avec le message d'erreur sauvgarde dans un query string
+    // si non afficher préparer la requête d'insertion
+    // vérifier qu'elle s'execute correctement
+    // s'il y a une erreur afficher le message puis arrêter le programme avec la "die"
+    // si non se redériger à la page index qui devera insérer le message nouvellement inséré
 
-if(isset($_POST['envoyer'])) {
-    $utilisateur = mysqli_real_escape_string($link, $_POST['utilisateur']);
-    $commentaire = mysqli_real_escape_string($link, $_POST['message']);
-
-    //adapter le time zone à votre localisation
-    date_default_timezone_set('America/New_York');
-
-    $temps = date('H:i:s', time());
-    //Validation des champs avant execution de la requête
-
-    if(empty($utilisateur) || empty($commentaire)){
-        $erreur = "utilisateur ou message non valide";
-        header("Location : index.php?erreur=".urlencode($erreur));
-            exit();
-    } 
-    else {
-        $query = "INSERT INTO messages (utilisateur, contenu_message) VALUES ( '$utilisateur', '$commentaire')";
-        if(!mysqli_query($link,$query)){
-            die ('Erreur : '.mysqli_error($link));
-        } 
-        else {
-            header("Location : index.php");
-            exit();
-        } 
-    }
-}
 
 
